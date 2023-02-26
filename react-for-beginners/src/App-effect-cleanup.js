be0cba082769444389c27a9704fcb0baaf79c585
suelrin <dancerelrin@gmail.com>
2023-02-27 07:19:29 +0900

    todo list

---
+++ react-for-beginners/src/App-effect-cleanup.js
@@ -0,0 +1,41 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import {useState, useEffect} from "react";
+
+function Hello(){
+  /*function destryedFn(){
+    console.log("destroyed :(");
+  }
+
+  function effectFn(){
+    console.log("created :)");
+    return destryedFn;
+  }
+  useEffect(effectFn,[]);*/
+
+  useEffect(() => {
+    console.log('hi');
+    return () => console.log("bye");
+  }, []);
+
+  useEffect(function(){
+    console.log('hi');
+    return function(){
+      console.log('bye');
+    }
+  }, []);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+        {showing ? <Hello /> : null}
+        <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
+
+export default App;
