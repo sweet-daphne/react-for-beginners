c27dea1ba3c664bf7332083937c3452c9052f61f
suelrin <dancerelrin@gmail.com>
2023-03-06 07:32:00 +0900

    parameter

--- react-for-beginners/src/components/Movie.js
+++ react-for-beginners/src/components/Movie.js
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 
-function Movie({coverImage,title,summary,genres,id}){
+function Movie({id,coverImage,title,summary,genres}){
   return (
-    <div key={id}>
+    <div>
       <img src={coverImage} alt={title} />
       <h2>
-        <Link to="/movie">{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
       <p>{summary}</p>
       <ul>
@@ -19,6 +19,7 @@ function Movie({coverImage,title,summary,genres,id}){
 }
 
 Movie.propTypes = {
+  id : PropTypes.number.isRequired,
   coverImage : PropTypes.string.isRequired,
   title : PropTypes.string.isRequired,
   summary : PropTypes.string.isRequired,
